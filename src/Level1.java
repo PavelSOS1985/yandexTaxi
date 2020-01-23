a61931887e940d5267f5fb3df93cd8b7341ff9c2
PavelSOS <sawel@yandex.ru>
2020-01-23 17:28:09 +0800

    ver3

--- src/Level1.java
+++ src/Level1.java
@@ -5,9 +5,9 @@ public class Level1 {
         if (N <= 0 || track[0][0] >= L) return L;
         int distToTrafLights = track[0][0];
         int moment = track[0][0];
-        boolean trafLights = false;
+        boolean trafLights;
         int tempTimeOnTL;
-        // расчет времени по светофорам
+        // расчет времени по всем светофорам
         for (int i = 0; i < N; i++) {
             if (i != 0) {
                 if (track[i][0] < L) {
@@ -18,6 +18,7 @@ public class Level1 {
                 }
                 if (track[i][0] < track[i - 1][0]) break;
             }
+            trafLights = false;
             tempTimeOnTL = 0;
             while (tempTimeOnTL <= moment) {
                 for (int j = 1; j < 3; j++) {
